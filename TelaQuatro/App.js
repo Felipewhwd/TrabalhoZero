2cb0b2cd8fcdaaeeae76569c4e5d9b6df88e4cf1
Felipewhwd <lfelipesann@gmail.com>
2022-03-05 00:15:50 -0300

    TelaQuatro

--- TelaQuatro/App.js
+++ TelaQuatro/App.js
@@ -16,7 +16,7 @@ class app extends Comment{
     return(
       <SafeAreaView style = {style.conteiner}>
 
-        <StatusBar backgroundColor ='#C077C9' barStyle='light-content'>
+        <StatusBar backgroundColor ='#E6DB55' barStyle='light-content'>
 
           <View style ={Style.content} >
 
@@ -25,17 +25,17 @@ class app extends Comment{
           </View>
           <View style ={Style.content}>
             <image Style = {Style.image}>
-              source ={require('./assets/b974c1872c2d2d1a06b506666bc0b89f.jpg')}
+              source ={require('./iu.jpg')}
             </image>
             <Image Style = {Style.image}>
-              source ={require('./assets/b9bc049a372cd609a83e0cc2eacd7482.jpg')}
+              source ={require('./assets/ai.jpg')}
             </Image>
           </View>
 
 
           <TouchableOpacity style = {style.fab}>
 
-            <Ionicons name='ios-add' size ='{30}'color ='#C270BD'/>
+            <Ionicons name='ios-add' size ='{30}'color ='#D9D16C'/>
 
           </TouchableOpacity>
 
@@ -47,27 +47,27 @@ class app extends Comment{
   const Style = StyleSheet.create({
     conteiner:{
       flex:1,
-      backgroundColor: '#D9A0D5'
+      backgroundColor: '#EBDF3D'
     },
     title:{
-      marginTop:12,
-      paddingBottom:12,
-      fontSize: 27,
+      marginTop:14,
+      paddingBottom:14,
+      fontSize: 29,
       textAlign:'center',
-      color:'#D9A0D5'
+      color:'#D1C95C'
     },
     fab:{
       position:"absolute",
-      width: 65,
-      height: 65,
+      width: 66,
+      height: 66,
       backgroundColor:'#7A6179',
       alignItems: 'center',
       justifyContent:'center',
-      borderRadius:35,
-      right:20,
-      bottom:20,
-      elevation:4,
-      zIndex:11,
+      borderRadius:30,
+      right:23,
+      bottom:23,
+      elevation:2,
+      zIndex:10,
       shadowColor:'#003',
       shadowOpacity:0.2,
       shadowOffset:{
@@ -79,8 +79,8 @@ class app extends Comment{
       flex: 1,
       alignItems:'center',
       justifyContent:'center',
-      width:9,
-      height:9,
+      width:10,
+      height:11,
     }
   });
   }
